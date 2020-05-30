b87e5a1a57c9173dbd74c449be2d940c6ee21ba8
Phil Sharp <phil.j.sharp@outlook.com>
2020-05-30 11:51:46 +0100

    Only generate classes for V16 Schema

--- schemas/generateClasses.ps1
+++ schemas/generateClasses.ps1
@@ -2,8 +2,12 @@ $files = Get-ChildItem -Path C:\Users\phils\source\DarwinClient\schemas -Filter 
 
 $schemas = "xsd"
 foreach ($f in $files){
-    $schemas = $schemas + " " +  $f.Name; 
+    if($f.Name -notmatch 'rttiPPTSchema_v1[12345].xsd')
+    {
+        $schemas = $schemas + " " +  $f.Name; 
+    }
 }
+ 
 
 # Add dummy schema so output class file called schemaV16.cs
 # See https://stackoverflow.com/a/33906829/3805124
